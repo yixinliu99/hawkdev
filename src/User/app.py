11ddc52f3b47fe3f87d5cb9a1d66fe314c1b621e
KOYYADOONDYSAIVYSHNAVI <koyya@uchicago.edu>
2024-12-03 11:19:52 -0600

    user microservice

---
+++ src/User/app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from app.routes.user_routes import user_bp
+from db import db, mongo
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/userdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'supersecretkey'  # Use an environment variable in production
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/auction_site'
+
+# Initialize extensions
+#db = SQLAlchemy(app)
+CORS(app)
+
+# Register blueprints
+app.register_blueprint(user_bp, url_prefix="/api/users")
+db.init_app(app)
+mongo.init_app(app)
+
+
+# Global error handler
+@app.errorhandler(Exception)
+def handle_exception(e):
+    return {"message": str(e)}, 500
+
+if __name__ == "__main__":
+    # Initialize the database if not already created
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True, port=5001)
---
+++ src/User/app/models/user.py
@@ -0,0 +1,23 @@
+# app/models/user.py
+
+from flask_sqlalchemy import SQLAlchemy
+
+#db = SQLAlchemy()
+from db import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    user_type = db.Column(db.String(20), nullable=False) # "user" or "admin"
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+
