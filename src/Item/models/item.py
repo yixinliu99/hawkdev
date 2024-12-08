4df0b39928da2f3764ba86d64b764d72ce143795
Yixin Liu <ihsin.liu@outlook.com>
2024-12-08 12:31:01 -0600

    Add auction_id field to item

--- src/Item/models/item.py
+++ src/Item/models/item.py
@@ -2,7 +2,7 @@ from Item.consts.consts import ITEM_COLLECTION
 
 class Item:
     def __init__(self, user_id: str, starting_price: float, quantity: int, shipping_cost: float, description: str, flagged: bool = False,
-                 category: str = None, keywords: list[str] = None, item_id=None):
+                 category: str = None, keywords: list[str] = None, item_id=None, auction_id=None):
         self.id = item_id
         self.user_id = user_id
         self.starting_price = starting_price
@@ -12,18 +12,19 @@ class Item:
         self.flagged = flagged
         self.category = category
         self.keywords = keywords
+        self.auction_id = auction_id
 
     
     def to_dict(self):
         return {"user_id": self.user_id, "starting_price": self.starting_price, "quantity": self.quantity,
             "shipping_cost": self.shipping_cost, "description": self.description, "flagged": self.flagged,
-            "category": self.category, "keywords": self.keywords, "_id": self.id}
+            "category": self.category, "keywords": self.keywords,"auction_id" :self.auction_id, "_id": self.id}
 
     @staticmethod
     def from_dict(data):
         return Item(user_id=data["user_id"], starting_price=data["starting_price"], quantity=data["quantity"],
             shipping_cost=data["shipping_cost"], description=data["description"], flagged=data["flagged"] if "flagged" in data else False, category=data["category"] if "category" in data else None, keywords=data["keywords"] if "keywords" in data else None,
-            item_id=data["_id"] if "_id" in data else None)
+            auction_id=data["auction_id"] if "auction_id" in data else None, item_id=data["_id"] if "_id" in data else None)
 
     def create(self, dao):
         return dao.write_to_db(ITEM_COLLECTION, self.to_dict())
