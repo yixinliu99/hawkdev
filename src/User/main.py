39ab344b001fbe8bddcbbca0e1f00f503c3a2584
Yixin Liu <ihsin.liu@outlook.com>
2024-12-08 19:20:03 -0600

    Dockerize user service and make connection with

--- src/User/main.py
+++ src/User/main.py
@@ -34,4 +34,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 5001), debug=True)
